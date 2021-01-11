9037e66c7508bb9b763155f4665a2feb391bc3a1
SurajKGoyal <sgoyal275@gmail.com>
2021-01-12 00:58:12 +0530

    Initial commit

---
+++ app/src/main/java/com/heliushouse/talkingfragment/way_three/WayThreeActivity.kt
@@ -0,0 +1,12 @@
+package com.heliushouse.talkingfragment.way_three
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.heliushouse.talkingfragment.R
+
+class WayThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_way_three)
+    }
+}
---
+++ app/src/main/java/com/heliushouse/talkingfragment/way_two/WayTwoActivity.kt
@@ -0,0 +1,12 @@
+package com.heliushouse.talkingfragment.way_two
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.heliushouse.talkingfragment.R
+
+class WayTwoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_way_two)
+    }
+}
